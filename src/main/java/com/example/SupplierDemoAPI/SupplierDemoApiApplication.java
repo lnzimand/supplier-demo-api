fcc0feaf7c5463222c7a217c06b975eac11b8e36
Lebohang <Nzimande>
2021-04-01 22:42:22 +0200

    Supplier Demo API first entrance

---
+++ src/main/java/com/example/SupplierDemoAPI/SupplierDemoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SupplierDemoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierDemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplierDemoApiApplication.class, args);
+	}
+
+}
